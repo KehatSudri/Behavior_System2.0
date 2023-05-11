e6f2ff6d312043b8d2bcbfd310e1e57103938aea
Leonardo <liorodin@gmail.com>
2023-05-11 22:49:55 +0300

    DB updates part 7

--- Models/prepare_session_information.py
+++ Models/prepare_session_information.py
@@ -2,40 +2,41 @@ import os
 from pathlib import Path
 
 
-def prepare_session_information(ports,dependencies,trial_name,index,trials_in_session):
-    # print(ports)
+def prepare_session_information(ports, dependencies, trial_name, index, trials_in_session):
     input_ports = []
-    output_ports=[]
-    dependencies_string="None"
-    for port, type ,name in ports:
+    output_ports = []
+    dependencies_arr = []
+    for port, type, name in ports:
         if type == 'Input':
             input_ports.append(port)
         elif type == 'Output':
             output_ports.append(port)
-            parameters=trials_in_session[index+1][name]
+            parameters = trials_in_session[index + 1][name]
             parameters_string = str(parameters).replace(",", " ").replace("'", "")
             parameters_string = "[" + parameters_string[1:-1] + "]"
             output_ports.append(parameters_string)
-    if not input_ports :
-        input_ports_string ='None'
-    else:
-        input_ports_string = ",".join(input_ports)
-    if not output_ports :
-        output_ports_string ='None'
-    else:
-        print(output_ports)
-        output_ports_string = ",".join(output_ports)
     if dependencies:
-        dependencies_string = ','.join(['('+','.join(pair)+')' for pair in dependencies])
-
+        for pair in dependencies:
+            separator = ","
+            output_string = separator.join(pair)
+            dependencies_arr.append(output_string)
     configs_path = str(Path(__file__).parent.parent / 'config_files' / 'session_config.txt')
 
     with open(configs_path, "a") as file:
         file.write("Trial name : " + trial_name + "\n")
-        file.write("Dependencies\n" + dependencies_string + "\n")
-        file.write("Input Ports\n"+input_ports_string+"\n")
-        file.write("Output Ports\n" + output_ports_string+"\n")
+        file.write("Dependencies\n")
+        if len(dependencies_arr) > 0:
+            for dep in dependencies_arr:
+                file.write(dep + "\n")
+        else:
+            file.write("None\n")
+        file.write("Input Ports\n")
+        if len(input_ports) > 0:
+            for input in input_ports:
+                file.write(input + "\n")
+        else:
+            file.write("None\n")
+        file.write("Output Ports\n")
+        for output in output_ports:
+            file.write(output + "\n")
         file.write("\n")
-
-
-
