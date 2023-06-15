db3b4340246cbad462a9b62413822e40d5e95873
Kehat Sudri <kehat_sudri@intuit.com>
2023-06-15 22:02:15 +0300

    add configs of max trial time , and percent

--- Models/prepare_session_information.py
+++ Models/prepare_session_information.py
@@ -4,7 +4,7 @@ from Models.DB_INIT import DB
 
 
 def prepare_session_information(session_name, ports, dependencies, trial_name, index, trials_in_session, is_fixed_iti,
-                                repeats, isRandomOrder):
+                                repeats, isRandomOrder,MaxTime,Percent):
     input_ports = []
     output_ports = []
     dependencies_arr = []
@@ -22,15 +22,15 @@ def prepare_session_information(session_name, ports, dependencies, trial_name, i
 
     with open(configs_path, "a") as file:
         db = DB()
-        max_trial_time = db.get_max_trial_time(session_name)
-        file.write(str(max_trial_time[0]) + ",")
+        max_session_time = db.get_max_trial_time(session_name)
+        file.write(str(max_session_time[0]) + ",")
         iti_vals = db.get_iti_vals(session_name)
         if is_fixed_iti:
             file.write("(" + str(iti_vals[0]) + "),")
         else:
             file.write(str(iti_vals) + ",")
         file.write(str(isRandomOrder) + "\n")
-        file.write("Trial_name:" + trial_name + "\n" + repeats[int(index / 2)] + "\n")
+        file.write("Trial_name:" + trial_name + "\n" + repeats[int(index / 2)] + "," + MaxTime[int(index / 2)]+ "," + Percent[int(index / 2)]+ "\n")
         file.write("$Input Ports\n")
         if len(input_ports) > 0:
             for port in input_ports:
--- Views/random_order.py
+++ Views/random_order.py
@@ -38,13 +38,23 @@ class RandomOrderUi(object):
         # dialog.rejected.connect()
         self.trials_tableWidget = dialog.findChild(QtWidgets.QTableWidget, "trials_tableWidget")
         self.isRandomOrder = dialog.findChild(QtWidgets.QCheckBox, 'checkBox')
+        self.isRandomOrder.stateChanged.connect(self.checkbox_state_changed)
         for i in range (int(len(self.parent.trials_in_session)/2))   :
             self.trials_tableWidget.insertRow(i)
             self.trials_tableWidget.setItem(i, 0, QTableWidgetItem(self.parent.trials_in_session[i*2]))
             self.trials_tableWidget.setItem(i, 1, QTableWidgetItem("1"))
-
         return
-
+    def checkbox_state_changed(self):
+        if self.isRandomOrder.isChecked():
+            self.trials_tableWidget.setColumnCount(self.trials_tableWidget.columnCount() + 1)
+            column_index = self.trials_tableWidget.columnCount() - 1
+            header_item = QTableWidgetItem("Percent")
+            self.trials_tableWidget.setHorizontalHeaderItem(column_index, header_item)
+        else:
+            print("Checkbox unchecked")
+            self.trials_tableWidget.removeColumn(self.trials_tableWidget.columnCount() - 1)
+        # self.trials_tableWidget.add_column("New Column Name")
+        # print("rrrrrr")
     def get_total_num_of_trials(self):
         total_num_of_trials = self.total_num_of_trials_spinBox.value()
         self.parent.total_num = total_num_of_trials
@@ -71,17 +81,27 @@ class RandomOrderUi(object):
         is_fixed_iti = self.config_info[2]
         isRandomOrder=self.isRandomOrder.isChecked()
         repeats = []
+        MaxTime=[]
+        Percent=[]
         for row in range(self.trials_tableWidget.rowCount()):
             item = self.trials_tableWidget.item(row, 1)
+            item2 = self.trials_tableWidget.item(row, 2)
+
+            if isRandomOrder:
+                item3 = self.trials_tableWidget.item(row, 3)
+
             if item is not None:
                 repeats.append(item.text())
+                MaxTime.append(item2.text())
+                if isRandomOrder:
+                    Percent.append(item3.text())
         if not repeats:
             error_warning("Number of repetition should be at least 1")
             return
         for i in range(0, len(trials_in_session), 2):
             ports = (self.vm.get_ports(trials_in_session[i]))
             dependencies = self.vm.get_dependencies(trials_in_session[i])
-            prepare_session_information(session_name,ports,dependencies,trials_in_session[i],i,trials_in_session,is_fixed_iti,repeats,isRandomOrder)
+            prepare_session_information(session_name,ports,dependencies,trials_in_session[i],i,trials_in_session,is_fixed_iti,repeats,isRandomOrder,MaxTime,Percent)
         self.event_handler()
 
 
